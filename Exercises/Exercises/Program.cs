6038e6f7dcd3ad60e17993edee8323fe68d31cb1
Jaqueline Topan <jaqueline.rodrigues.topan@volvo.com>
2023-06-23 19:53:09 +0200

    Adding 1st exercise from C# .Net training.

--- Exercises/Exercises/Program.cs
+++ Exercises/Exercises/Program.cs
@@ -4,7 +4,38 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Enter the number of the day (1 to 7):");
+
+            var dayNumber = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine(dayNumber);
+
+            switch (dayNumber)
+            {
+                case 1:
+                    Console.WriteLine("Monday");
+                    break;
+                case 2:
+                    Console.WriteLine("Tuesday");
+                    break;
+                case 3:
+                    Console.WriteLine("Wednesday");
+                    break;
+                case 4:
+                    Console.WriteLine("Thursday");
+                    break;
+                case 5:
+                    Console.WriteLine("Friday");
+                    break;
+                case 6:
+                    Console.WriteLine("Saturday");
+                    break;
+                case 7:
+                    Console.WriteLine("Sunday");
+                    break;
+                default:
+                    Console.WriteLine("Invalid day number!");
+                    break;
+            }
         }
     }
 }
